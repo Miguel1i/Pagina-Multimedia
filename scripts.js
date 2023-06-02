86fdeb50208d2288d49f7d7ecbc98b4c5f02100e
Miguel <miguelrego14@gmail.com>
2023-06-02 09:36:00 +0000

    Ending

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
-function playAudio () {
-    var audio = new Audio("./audio/quack.mp3");
-    audio.play();
+var faq = document.getElementsByClassName("faq-page");
+var i;
+
+for (i = 0; i < faq.length; i++) {
+  faq[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var body = this.nextElementSibling;
+    if (body.style.display === "block") {
+      body.style.display = "none";
+    } else {
+      body.style.display = "block";
+    }
+  });
 }
